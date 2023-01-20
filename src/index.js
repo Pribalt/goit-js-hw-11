1089ae2764bd632c72b7096a6822fd5d55a2c502
Pribalt <vitalisvitalik@gmail.com>
2023-01-21 00:07:36 +0200

    change3

--- src/index.js
+++ src/index.js
@@ -1,52 +1,6 @@
 import './css/styles.css';
-import axios from 'axios';
-import Notiflix from 'notiflix';
-import { getPhoto } from './getPhoto';
+import { onSearch } from './js/onSearch';
 
 const searchFormEl = document.querySelector('#search-form');
-const listEl = document.querySelector('.js-photos');
 
-searchFormEl.addEventListener('submit', onSubmit);
-
-function onSubmit(e) {
-  e.preventDefault();
-
-  const {
-    searchQuery: { value: searchQuery },
-  } = e.currentTarget.elements;
-
-  console.log(searchQuery);
-
-  getPhoto(searchQuery).then(data => createListHTML(data.hits));
-}
-
-function createListHTML(data) {
-  listEl.innerHTML = createMarkup(data);
-}
-
-function createMarkup(arr) {
-  console.log(arr);
-  return arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<li data-photoId="1">
-           <img src="${(webformatURL, largeImageURL)}" alt="${tags}" />
-            <div>
-               <p><span>likes</span>${likes}</p>
-               <p><span>views</span>${views}</p>
-               <p><span>comments</span>${comments}</p>
-               <p><span>downloads</span>${downloads}</p>
-            </div>
-          </li>`;
-      }
-    )
-    .join();
-}
+searchFormEl.addEventListener('submit', onSearch);
