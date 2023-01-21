ba737471221a596b5ecbacc54f169490fa570ef9
Pribalt <vitalisvitalik@gmail.com>
2023-01-21 22:14:17 +0200

    change4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,69 @@
 import './css/styles.css';
-import { onSearch } from './js/onSearch';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { NewApiService } from './js/getPhotoAPI';
+import shablonMarkup from './templates/photo-card.hbs';
 
 const searchFormEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
 
 searchFormEl.addEventListener('submit', onSearch);
+loadMoreBtnEl.addEventListener('click', onLoadMore);
+
+const newApiService = new NewApiService();
+
+function onSearch(e) {
+  e.preventDefault();
+
+  newApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  newApiService.resetPage();
+
+  if (!newApiService.query) {
+    return;
+  }
+
+  newApiService
+    .fetchPhoto()
+    .then(({ totalHits, hits }) => {
+      if (totalHits === 0) {
+        loadMoreBtnEl.hidden = true;
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+
+      if (totalHits) {
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      }
+
+      clearShablonGallery();
+      appendShablonMarkup({ hits });
+
+      loadMoreBtnEl.hidden = false;
+    })
+    .catch(error => console.log(error));
+}
+
+function onLoadMore() {
+  newApiService
+    .fetchPhoto()
+    .then(({ hits }) => {
+      appendShablonMarkup({ hits });
+    })
+    .catch(error => console.log(error));
+}
+
+function appendShablonMarkup(hits) {
+  galleryEl.insertAdjacentHTML('beforeend', shablonMarkup(hits));
+}
+
+function clearShablonGallery() {
+  galleryEl.innerHTML = '';
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
---
+++ src/js/getPhotoAPI.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '32953111-1e0841424fa9175caa9620a35';
+
+class NewApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchPhoto() {
+    console.log(this);
+
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&per_page=40&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`
+    );
+    this.page += 1;
+
+    return response.data;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
+
+export { NewApiService };
